d6b12339e60e8d21b099d9bd357202afb8a209c1
Arthur Araújo <webarthur@gmail.com>
2016-09-07 15:12:58 -0300

    add map file

--- compile.sh
+++ compile.sh
@@ -1,3 +1,3 @@
 #/bash/bin
 sass --sourcemap=none sass/helpers.scss css/helpers.css
-sass --sourcemap=none --style compressed sass/helpers.scss css/helpers.min.css
+sass --style compressed sass/helpers.scss css/helpers.min.css
